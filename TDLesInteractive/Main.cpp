4cb6403040a3ccf300a3705f5b728daf386ddfd8
spirgari <sorin.pirgari@lyceeastier.com>
2024-09-10 09:23:07 +0200

    Add project files.

---
+++ TDLesInteractive/Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+	/* Exercices 1 A
+	int nombre = 400000;
+	cout << left << setw(15) << nombre <<'|'<< endl;
+	return 0; */
+
+	/*B
+	double nombre;
+	cout << "Entrer votre chiffre: ";
+	cin >> nombre;
+	cout << fixed << setprecision(3) << nombre << endl;
+	*/
+
+	/*C
+	int nombre = 200;
+	cout << nombre << endl;
+	cout << showpos << nombre << endl; */
+	
+	/*D
+	int nombre = 100;
+	cout << hex << nombre << endl;
+	*/
+
+	/*E*/
+
+	
+
+
+
+	
+	
+	
+
+
+
+	
+
+}
